1788fe3ea02c4d362e2386555f1ff6de75b528c7
ThamerAyachi <thamerjak3@gmail.com>
2023-09-28 09:45:17 +0100

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""wait_n function"""
+
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax")
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n function"""
+    listTime: List[float] = []
+
+    for i in range(n):
+        listTime.append(await wait_random(max_delay))
+
+    return sorted(listTime)
