09e385ac2b9dcad368cf441d4ad9d792e5a1dfc8
ThamerAyachi <thamerjak3@gmail.com>
2023-09-28 09:12:29 +0100

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""  function sum_mixed_list """
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float|int]) -> float:
+    """  function sum_mixed_list """
+    return sum(mxd_lst)
