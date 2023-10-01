330cf79d3dfe258bb578f7c35a51b2062fa0a8c9
ThamerAyachi <thamerjak3@gmail.com>
2023-10-01 12:35:37 +0100

    fix bugs

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,32 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """LRU Caching"""
-
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class LRUCache (BaseCaching):
-    """LRU Caching"""
+class LRUCache(BaseCaching):
+    """class LRUCache that inherits
+    from BaseCaching and is a caching system"""
 
     def __init__(self):
-        """Initiliaze
-        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key and item:
+        """Must assign to the dictionary """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {discarded_key}")
             self.cache_data[key] = item
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
-            leastKey = next(iter(self.cache_data))
-            print(f"DISCARD: {leastKey}")
-            del self.cache_data[leastKey]
-        if not key or not item:
-            pass
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key and key in self.cache_data:
-            return self.cache_data[key]
+        """Must return the value in self.cache_data"""
+        if key is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+                return self.cache_data[key]
         return None
--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,32 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """MRU Caching"""
-
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRU Caching"""
+    """MRUCache that inherits from
+    BaseCaching and is a caching system:"""
 
     def __init__(self):
-        """Initiliaze
-        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
-        """
-        if key and item:
+        """Must assign to the dictionary """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data.keys()))
+                print(f"DISCARD: {discarded_key}")
+                self.cache_data.popitem(last=False)
             self.cache_data[key] = item
-            if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
-                leastKey = next(iter(self.cache_data))
-                print(f"DISCARD: {leastKey}")
-                del self.cache_data[leastKey]
-        else:
-            pass
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key and key in self.cache_data:
-            return self.cache_data[key]
+        """Must return the value in self.cache_data"""
+        if key is not None:
+            if key in self.cache_data:
+                value = self.cache_data[key]
+                self.cache_data.move_to_end(key)
+                return value
         return None
