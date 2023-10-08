622232d572ae18973fddf8b85bcece249905b1ab
ThamerAyachi <thamerjak3@gmail.com>
2023-10-08 16:31:00 +0100

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
             return None
         Base64 = re.split(' ', authorization_header)
         return Base64[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header Method"""
+
+        if (base64_authorization_header is None or
+                (type(base64_authorization_header) != str)):
+            return None
+        try:
+            decodeBytes = base64.b64decode(base64_authorization_header)
+            decodedStr = decodeBytes.decode("utf-8")
+            return decodedStr
+        except ValueError:
+            return None
