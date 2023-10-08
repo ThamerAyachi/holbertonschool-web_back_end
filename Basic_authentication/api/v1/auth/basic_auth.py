f12bc3df6c58a7aeb902ac002cc61b3b8764afef
ThamerAyachi <thamerjak3@gmail.com>
2023-10-08 16:29:31 +0100

    task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,21 @@
 """ Basic Auth """
 
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """ Basic Auth Class"""
+
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """extract_base64_authorization_header Method"""
+
+        if authorization_header is None or (type(authorization_header) != str):
+            return None
+        if authorization_header.startswith("Basic ") is False:
+            return None
+        Base64 = re.split(' ', authorization_header)
+        return Base64[1]
