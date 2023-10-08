ea58945ba0cd6e69801ea579b9e25075da14b453
ThamerAyachi <thamerjak3@gmail.com>
2023-10-08 13:07:21 +0100

    remove extra lines

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,11 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """filter_datum function"""
     for field in fields:
         message = re.sub(
