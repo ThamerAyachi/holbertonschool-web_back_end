db07abf9b36b37471c8b1c0ea580e5e54fa68542
ThamerAyachi <thamerjak3@gmail.com>
2023-10-15 20:15:31 +0100

    task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -12,3 +12,15 @@ AUTH = Auth()
 def bienvenue():
     """bienvenue method"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"])
+def register():
+    """register end-point"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
