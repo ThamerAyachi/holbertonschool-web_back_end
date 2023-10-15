bfd2696bebb3bcfac11f0c697528d2aedcf1e79c
ThamerAyachi <thamerjak3@gmail.com>
2023-10-15 20:32:00 +0100

    task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -38,3 +38,15 @@ def login():
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """logout method"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
