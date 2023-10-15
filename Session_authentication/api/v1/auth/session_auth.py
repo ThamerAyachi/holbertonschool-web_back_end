f9ca45d3bd5770ee0bf6011f0c00985a1549bbb0
ThamerAyachi <thamerjak3@gmail.com>
2023-10-15 18:40:48 +0100

    task 8

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -33,3 +33,17 @@ class SessionAuth(Auth):
 
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """destroy_session that deletes
+        the user session / logout"""
+
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        if not self.user_id_for_session_id(session_id):
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
