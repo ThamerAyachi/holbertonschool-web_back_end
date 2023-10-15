e479f44f070aeb8f150fd722f4731c4c5f330b5a
ThamerAyachi <thamerjak3@gmail.com>
2023-10-15 20:43:16 +0100

    task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -73,3 +73,20 @@ def get_reset_password_token():
         return jsonify({"email": email, "reset_token": token})
     else:
         abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """update_password method"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
