880b859370fec9936ea94390b55364980d625ca5
ThamerAyachi <thamerjak3@gmail.com>
2023-10-20 11:40:15 +0100

    task 4

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Test Client File"""
+
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import patch
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Github Org Client Class"""
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    def test_org(self, test_org, mock_json):
+        """Test Org Method"""
+        client = GithubOrgClient(test_org)
+        client.org()
+        mock_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{test_org}"
+        )
