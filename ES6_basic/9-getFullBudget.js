49f51f017d50a44090840247da5e70e19123e789
ThamerAyachi <thamerjak3@gmail.com>
2023-11-22 22:53:50 +0100

    task 9

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,11 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const fullBudget = {
+    budget: getBudgetObject(income, gdp, capita),
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
