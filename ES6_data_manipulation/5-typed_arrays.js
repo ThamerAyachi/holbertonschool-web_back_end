230ba86a0a6189d50700c70baa367fdf0bb50dc6
ThamerAyachi <ThamerAyachi@github.com>
2023-12-06 11:33:52 +0100

    try fix 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) throw new Error('Position outside range');
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const buffer = new ArrayBuffer(length);
-  const array = new Int8Array(buffer);
-  array[position] = value;
-  return array;
+
+  const int8Array = new Int8Array(buffer);
+
+  int8Array[position] = value;
+
+  return buffer;
 }
