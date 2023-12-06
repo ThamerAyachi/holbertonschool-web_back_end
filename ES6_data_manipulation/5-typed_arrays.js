7971bef485c86a73491e1fbbb68458ee06cc9d68
ThamerAyachi <ThamerAyachi@github.com>
2023-12-06 11:39:20 +0100

    task 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  const int8Array = new Int8Array(length);
+  int8View[position] = value;
 
-  int8Array[position] = value;
-
-  return int8Array;
+  return new DataView(buffer);
 }
