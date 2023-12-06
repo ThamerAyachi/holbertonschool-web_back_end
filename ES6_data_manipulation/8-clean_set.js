bb2f319ad96040b57df5c137cd7c56cf04943462
ThamerAyachi <ThamerAyachi@github.com>
2023-12-06 12:10:48 +0100

    fix style

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,11 @@
 const cleanSet = (set, startString) => {
-  if (
-    typeof set !== 'object' ||
-    typeof startString !== 'string' ||
-    startString.length === 0
-  ) {
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
   let result = [];
   for (const element of set) {
-    if (
-      element &&
-      typeof element === 'string' &&
-      element.startsWith(startString)
-    ) {
+    if (element && typeof element === 'string' && element.startsWith(startString)) {
       result.push(element.slice(startString.length));
     }
   }
