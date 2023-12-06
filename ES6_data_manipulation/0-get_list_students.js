f0377f371c038bd6feef0c4e7d083212b89216a9
ThamerAyachi <ThamerAyachi@github.com>
2023-12-06 10:52:15 +0100

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+const getListStudents = () => [
+  {
+    id: 1,
+    fistName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    fistName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    fistName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+export default getListStudents;
