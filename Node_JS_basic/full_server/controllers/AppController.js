25b43cf04a9fc24effc0494793511c499985e3f6
ThamerAyachi <thamerjak3@gmail.com>
2023-12-19 09:57:18 +0100

    task 8

---
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+class AppController {
+  static getHomepage(request, response) {
+    return response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
