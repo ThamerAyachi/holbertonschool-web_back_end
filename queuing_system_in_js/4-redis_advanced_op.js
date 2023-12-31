c48f99daa4f755c221478d312e8ac665bd4036b7
ThamerAyachi <thamerjak3@gmail.com>
2023-12-31 10:03:16 +0100

    task 4

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,57 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const hsetAsync = promisify(client.hset).bind(client);
+
+const data = [
+  {
+    key: 'Portland',
+    value: 50,
+  },
+  {
+    key: 'Seattle',
+    value: 80,
+  },
+  {
+    key: 'New York',
+    value: 20,
+  },
+  {
+    key: 'Bogota',
+    value: 20,
+  },
+  {
+    key: 'Cali',
+    value: 40,
+  },
+  {
+    key: 'Paris',
+    value: 2,
+  },
+];
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setHolbertonSchools = () => {
+  for (const item of data) {
+    client.hset('HolbertonSchools', item.key, item.value, redis.print);
+  }
+};
+
+const displayHolbertonSchools = () => {
+  client.hgetall('HolbertonSchools', (err, result) => {
+    if (!err) console.log(result);
+  });
+};
+
+(() => {
+  setHolbertonSchools();
+  displayHolbertonSchools();
+})();
