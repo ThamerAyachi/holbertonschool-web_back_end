c93cb5714a21b29a899c4d481e0f940966090189
ThamerAyachi <thamerjak3@gmail.com>
2023-12-31 11:03:24 +0100

    task 9

---
+++ queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,27 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+const queueName = 'push_notification_code_2';
+
+const blacklisted = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  const total = 100;
+  job.progress(0, total);
+
+  if (blacklisted.includes(phoneNumber)) {
+    done(Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+
+  job.progress(50, total);
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+  done();
+};
+
+queue.process(queueName, 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
